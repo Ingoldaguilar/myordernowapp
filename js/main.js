a22274358f799451cb105089a9b8afe49334272a
ingold <ingoldaguilar45@gmail.com>
2022-02-01 18:45:45 -0600

    creamos la parte de javascript para el modal

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//dom
+let modal = document.querySelector('.modal');
+let body = document.querySelector('.body');
+let header = document.querySelector('#header');
+
+//btns
+let closeBtn = document.querySelector('#close');
+let showBtn = document.querySelector('#show');
+
+showBtn.addEventListener('click', function () {
+    modal.classList.remove('hide');
+    header.classList.add('hide');
+    body.classList.add('overlay');
+})
+
+
+closeBtn.addEventListener('click', function () {
+    modal.classList.add('hide');
+    header.classList.remove('hide');
+    body.classList.remove('overlay');
+})
+
